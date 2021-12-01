f301ae4801678e0f4e3e5f9dfd2d104c78162f9b
mukesh <sahm83381@gmail.com>
2021-12-01 13:24:34 +0545

    Lab eorks from Humagain sir

---
+++ lab3.py
@@ -0,0 +1,6 @@
+#Volume of cuboid 
+l =int(input("enter the length in cm"))
+b =int(input("enter the breadth in cm"))
+h =int(input("enter the height in cm"))
+V = l*b*h
+print("volume of cuboid is {} cm\u00b3".format(V))
---
+++ lab5.py
@@ -0,0 +1,5 @@
+#Area of circle
+#from diameter
+d = int(input("enter the diameter of a circle"))
+A=22/7*d**2/4
+print("the area of cicle is:", A)
