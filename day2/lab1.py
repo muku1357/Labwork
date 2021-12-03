279a84a3b817ff3fc343d0a2c3a8fd867a114802
mukesh <sahm83381@gmail.com>
2021-12-03 13:33:29 +0545

    Labwork

---
+++ day2/lab1.py
@@ -0,0 +1,8 @@
+#convert second into day,hrsand minute
+a = 86400
+day = a/(60*60*24)
+hrs = a/(60*60)
+min = a/60
+print( "the day is:",day)
+print("the hrs is:",hrs)
+print("the min is:",min)
---
+++ day2/lab2.py
@@ -0,0 +1,6 @@
+
+#BMI=mass in kg/(height in m)2
+M= int(input("enter the mass of person in kg"))
+H = int(input("enter the height of a person in meter"))
+BMI = (M / (H*H))
+print(f"the BMI value of a person is {BMI}")
