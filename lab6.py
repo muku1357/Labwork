22998f86a9f6c835cbc70dca6d22aa8407e80d3e
mukesh <sahm83381@gmail.com>
2021-12-03 12:05:05 +0545

    labwork

---
+++ lab6.py
@@ -0,0 +1,9 @@
+#add even numbers of first  positive 10 integer number
+n =[2,4,6,8,10]
+A = sum(n) 
+print(A)
+
+#add odd numbers of first possitive 10 integer number
+m = [1,3,5,7,9]
+B = sum(m)
+print(B)
