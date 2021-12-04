eb42876428fad78873974756d7b77ac292ef409d
mukesh <sahm83381@gmail.com>
2021-12-04 21:42:50 +0545

    labwork

--- day2/lab4.py
+++ day2/lab4.py
@@ -1,5 +1,13 @@
-s =("the speed is :", 25)
-t = ("the taken is :", 2)
-distance = (25/60*60)*(2*60)
-print(distance)
+# N students take K apples and distribute them among each other evenly. The remaining
+#(the indivisible) part remains in the basket. How many apples will each single student
+#get? How many apples will remain in the basket? The program reads the numbers N and
+#K. It should print the two answers for the questions above.
+
+from typing import no_type_check_decorator
+
+
+noof_students  = int(input("no of students getting apple"))
+noof_apple = int(input("no of apples remaning in the basket"))
+print(noof_apple//noof_students)
+print(noof_apple% noof_students)
 
