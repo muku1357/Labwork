fef92d71af468d4d76c4e58d1a97cc6d96bd3e67
mukesh <sahm83381@gmail.com>
2021-12-21 20:05:37 +0545

    labwork

--- day5/lab2.py
+++ day5/lab2.py
@@ -2,8 +2,8 @@
 import random
 target_num, guess_num = random.randint(1,10), 0
 while target_num != guess_num:
-    guess_num=int(input("guess a number between 1 and 10 untill you get it right:"))
-print("well guessed")
+    guess_num = int(input("guess a number between 1 and 10 untill you get it right:"))
+print("well guessed !")
 
 # target_num=random.randint(1,10)
 # guess_number=0
--- day5/lab6.py
+++ day5/lab6.py
@@ -1,4 +1,4 @@
-#Write a Python program to count the number of even andodd numbers from a series of numbers.
+#Write a Python program to count the number of even and odd numbers from a series of numbers.
 
 numbers = (11, 22, 33, 44, 55, 66, 77, 88, 89)
 count_odd = 0
@@ -10,3 +10,23 @@ for x in numbers:
     	     count_odd+=1
 print("Number of even numbers :",count_even)
 print("Number of odd numbers :",count_odd)
+#WAP to print to count the number of even or odd number from the series of number
+l = [2,4,6,10, 21, 4, 45, 66, 93, 1,11,31] 
+
+even, odd= 0, 0
+
+for i in l: 
+
+    if i % 2 == 0: 
+
+        even += 1
+
+    else: 
+
+        odd+= 1          
+
+print("Even : ", even) 
+
+print("Odd : ", odd)
+
+ 
